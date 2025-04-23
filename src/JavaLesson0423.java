ce1a817de4354bede8e8af238b031f809f06694a
Sakura <s.shioda@a-force.co.jp>
2025-04-23 18:00:37 +0900

    commit

---
+++ src/JavaLesson0423.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class JavaLesson0423 {
+	public static void main(String[]args) {
+		List<String> collectionlist =new ArrayList<String>();
+		int [] numbers= {1,2,3,4,5};
+		for(int i =numbers.length -1; i >=0; i--) {
+			System.out.println(numbers[i]);
+		
+		Set<String> set = new HashSet<String>();
+
+		}
+		
+		}
+		
+		
+		
+			
+		
+	}
